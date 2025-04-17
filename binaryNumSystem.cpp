0b138f34c1397920c32ccf194e6d22d9af9c2317
zaidahmedkhan <zaidkhan200415@gmail.com>
2025-04-17 17:17:49 +0500

    binary number systems

---
+++ binaryNumSystem.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int decToBinary(int decNum)
+{
+
+    int ans = 0, pow = 1;
+    while (decNum > 0)
+    {
+        int rem = decNum % 2;
+        decNum /= 2;
+        ans += (rem * pow);
+        pow *= 10;
+    }
+
+    return ans;
+}
+
+int main()
+{
+
+    int decNum = 50;
+    cout << decToBinary(decNum) << endl;
+    return 0;
+}
